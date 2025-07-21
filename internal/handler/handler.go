08678e37457fd260ba2c011a7167dbd7784bc4f9
PraneethVR10 <Praneethvr03@gmail.com>
2025-07-22 00:31:23 +0530

    Added the GetStudentID handler func

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -17,13 +17,25 @@ var records = []model.Record{
 
 // write the logic for how many handlers do you want to have
 
-func GetAllStudents(c *gin.Context) {
+func GetAllStudents(c *gin.Context) { // Uses GET
 
 	c.IndentedJSON(http.StatusOK, records)
 
-} // Uses GET
+}
 
-//func GetStudentID() // Uses GET
+func GetStudentID(c *gin.Context) { // Uses GET
+	id := c.Param("id")
+
+	// Loop over the list of albums, looking for
+	// an album whose ID value matches the parameter.
+	for _, a := range records {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
 
 //func AddStudent() // Uses POST
 
--- main.go
+++ main.go
@@ -1,17 +1,14 @@
 package main
 
 import (
-	"math/rand"
-	"time"
-
 	"github.com/PraneethVR10/RESTful-API/internal/handler"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
 	r := gin.Default()
 	r.GET("/students", handler.GetAllStudents)
+	r.GET("/students/:id", handler.GetStudentID)
 
 	r.Run(":3000")
 }
