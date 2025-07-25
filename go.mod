3021c2efcd24e7d16242432864be4b6ea2c2be5b
PraneethVR10 <Praneethvr03@gmail.com>
2025-07-25 16:34:22 +0530

    updated the GetAllStudents handler to fetch the data from the database

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.23.2
 
 require (
 	github.com/gin-gonic/gin v1.10.1
+	github.com/google/uuid v1.6.0
 	github.com/jackc/pgx/v5 v5.7.5
 	github.com/joho/godotenv v1.5.1
 )
--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -7,19 +7,21 @@ import (
 
 	"github.com/PraneethVR10/RESTful-API/internal/db"
 	"github.com/PraneethVR10/RESTful-API/internal/model"
+	"github.com/google/uuid"
 
 	"github.com/gin-gonic/gin"
 )
 
 var records = []model.Record{
-	{ID: "1", Name: "Praneeth", AdmissionNum: rand.Intn(200)},
-	{ID: "2", Name: "Sonu", AdmissionNum: rand.Intn(200)},
-	{ID: "3", Name: "VR", AdmissionNum: rand.Intn(200)},
+	{ID: uuid.New().String(), Name: "Praneeth", AdmissionNum: rand.Intn(200)},
+	{ID: uuid.New().String(), Name: "Sonu", AdmissionNum: rand.Intn(200)},
+	{ID: uuid.New().String(), Name: "VR", AdmissionNum: rand.Intn(200)},
 }
 
 // write the logic for how many handlers do you want to have
 
 func InsertData(c *gin.Context) {
+	var insertedRecords []model.Record
 	for _, student := range records {
 		_, err := db.DB.Exec(
 			context.Background(), "INSERT INTO students (id, name, admission_num) VALUES ($1, $2, $3)",
@@ -31,14 +33,34 @@ func InsertData(c *gin.Context) {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to insert", "details": err.Error()})
 			return
 		}
+		insertedRecords = append(insertedRecords, student)
 	}
-	c.JSON(http.StatusCreated, gin.H{"message": "Records inserted successfully"})
+	c.IndentedJSON(http.StatusOK, insertedRecords)
+
 }
 
 func GetAllStudents(c *gin.Context) { // Uses GET
 
-	c.IndentedJSON(http.StatusOK, records)
+	rows, err := db.DB.Query(context.Background(), "SELECT id, name, admission_num FROM students")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch data", "details": err.Error()})
+		return
+	}
+	defer rows.Close()
 
+	var students []model.Record
+
+	for rows.Next() {
+		var student model.Record
+		err := rows.Scan(&student.ID, &student.Name, &student.AdmissionNum)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error scanning row", "details": err.Error()})
+			return
+		}
+		students = append(students, student)
+	}
+
+	c.IndentedJSON(http.StatusOK, students)
 }
 
 func GetStudentID(c *gin.Context) { // Uses GET
